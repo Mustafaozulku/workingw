7fae0f242ff79c367b6871b76456e823b733f45e
Damla <Damla@LAPTOP-OFAHLJQ9>
2023-03-09 12:53:43 +0300

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+﻿select * from Products where CategoryID = 1 order by UnitPrice desc 
+
+select count(*) from Products
+
+select CategoryID,count(*) from Products where UnitPrice>20 group by CategoryID having count(*)<10
+
+ select Products.ProductName, Products.UnitPrice, Products.UnitPrice, Categories.CategoryName
+ from Products inner join Categories
+ on Products.CategoryID=Categories.CategoryID
+ where Products.UnitPrice>10
+
+ select * from Products p left join [Order Details] od
+ on p.ProductID = od.OrderID
+
+
+ select * from Customers c left join Orders o
+ on c.CustomerID = o.CustomerID
+ where o.CustomerID is null
+
+ select * from Products p inner join Categories c
+ on p.CategoryID = c.CategoryID 
